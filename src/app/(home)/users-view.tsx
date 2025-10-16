9aefde57d90583755913e0d2695edec543a9d231
Dylan Barduzzi <dlbarduzz@gmail.com>
2025-10-15 21:22:20 -0500

    fix - check for users data

--- src/app/(home)/users-view.tsx
+++ src/app/(home)/users-view.tsx
@@ -6,6 +6,11 @@ import { useSuspenseQuery } from "@tanstack/react-query"
 export function UsersView() {
   const trpc = useTRPC()
   const users = useSuspenseQuery(trpc.getUsers.queryOptions())
+
+  if (!users || !users.data) {
+    return null
+  }
+
   return (
     <div className="mt-4 bg-gray-200 max-w-max p-4">
       <pre>{JSON.stringify(users.data, null, 2)}</pre>
